4aee13c7ffd654e4c56c5833e55163fcc95bd528
mahdishirvanea <mahdishirvanea@gmail.com>
2023-04-02 14:13:51 +0330

    embedding1

--- transformer_blocks_test.py
+++ transformer_blocks_test.py
@@ -1,3 +1,14 @@
-from transformer_blocks import *
 
-test_multi_head_attention()
+
+def get_embedding(path: str):
+    vocab, embedding = list(), list()
+    with open(f'{path}', 'rt') as f:
+        full_content = f.read().strip().split('\n')
+    for i in range(len(full_content)):
+        i_word = full_content[i].split(' ')[0]
+        i_embedding = [float(val) for val in full_content[i].split(' ')[1:]]
+        vocab.append(i_word)
+        embedding.append(i_embedding)
+
+
+get_embedding("./word2vec_eng/glove.6B.100d.txt")
